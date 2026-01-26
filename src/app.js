1e079a3e15b0f52bab2f3f21b52416eb43ee2eca
Razim <razim@Mac.lan>
2026-01-25 23:21:27 -0600

    fixedreturn-presignedurl

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const auth = require("./middleware/auth.middleware");
 const fileRoutes = require("./routes/files.routes"); 
+const cors = require("cors");
+
+
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+}));
+
+app.options("*", cors());
 
 app.get("/health", (_req, res) => {
   res.status(200).json({
